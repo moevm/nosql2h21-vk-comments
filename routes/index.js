14f8747e6602051dbd3e35e7b216abf8f3e69c3d
kera0106 <01062000kK>
2021-09-16 23:44:16 +0300

    Connected to MongoDB Atlas, create helloworld app

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose')
+const {use} = require("express/lib/router");
+const stream = require("stream");
 require('dotenv').config()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: process.env.VAR });
+mongoose.connect('mongodb+srv://user:vkspampassword@cluster0.zcbgb.mongodb.net/vk_spam?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+}).then(() => console.log('Mogno connected successful')).catch((err) => console.log(err))
+
+
+const HelloSchema = new mongoose.Schema({
+  string: {
+    type: String
+  }
+})
+
+const Hello = mongoose.model('hello', HelloSchema)
+
+router.get('/addHelloString', (req, res) => {
+  hello = new Hello({
+     string: 'Hello World'
+    }
+  )
+  hello.save().then(() => res.send('saved'))
+})
+
+router.get('/', (req, res) => {
+  Hello.find().then((string, err) => {
+       res.render('index', { title: string[0].string });
+  })
 });
 
 module.exports = router;
