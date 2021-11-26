c0a129001195d65fdcdfff653b15c973ad11815e
yogik10 <Yogik_10@bk.ru>
2021-11-26 21:52:22 +0300

    phrase page stylized

--- client/src/components/Main/Phrase/Phrase.js
+++ client/src/components/Main/Phrase/Phrase.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import withRouter from "../../../utils/withRouter";
 import axios from "axios";
-import cs from "../../common/common.module.css"
+import styles from "./Phrase.module.css"
 import Table from "../../common/Table/Table";
 
 class Phrase extends React.Component {
@@ -39,24 +39,22 @@ class Phrase extends React.Component {
     render() {
         if (this.state.isLoaded) {
             return (
-            <div>
-                <div className={cs.center}>
-                    <div className={cs.title}>{this.state.phraseText}</div>
-                </div>
-                <div className={cs.mt10}>
+            <>
+                <div className={styles.title}>{this.state.phraseText}</div>
+                <div className={styles.table_wrap}>
                     <Table headers={[
                         { title: 'Имя комментатора', style: {'max-width': '250px'}, key: row => row.user.first_name
                                 + ' ' + row.user.last_name, link: row => `/users/${row.user['_id']}`},
-                        { title: 'Время', key: row => row['time']},
+                        { title: 'Время', style: {'width': '82px'}, key: row => row['time']},
                         { title: 'Дата', key: row => (new Date(row['date'])).toLocaleDateString()},
                         { title: 'Комментарии', style: {'max-width': '450px'}, key: row => row['text']},
-                        { title: 'Количество лайков', style: {'max-width': '100px'},
+                        { title: 'Количество лайков', style: {'max-width': '110px'},
                             key: row => row['likes_count'],
                             link: row => `/comment/${row.comment_id}`
                         }
                     ]} rows={this.state.comments}/>
                 </div>
-            </div>
+            </>
             )
         } else {
             return <></>
